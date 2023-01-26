e48795710eb2f5d60f1b0711643146010a9a2567
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-26 06:09:44 +0600

    Add chart

---
+++ Client/Controllers/ChartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers;
+
+//[Area("Guest")]
+public class ChartController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+}
