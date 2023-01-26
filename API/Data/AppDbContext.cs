8c7a56b78c6b98d97fea0b5d11766fae68064144
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-26 22:31:13 +0600

    Add AddDbcontext

---
+++ API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+}
