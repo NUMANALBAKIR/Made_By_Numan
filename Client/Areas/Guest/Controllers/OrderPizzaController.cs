f2384a8da593cc2d4a407eaf01bd233e97647b52
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-26 23:58:37 +0600

    Add pizza UI

---
+++ Client/Areas/Guest/Controllers/OrderPizzaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Areas.Guest.Controllers;
+
+[Area("Guest")]
+public class OrderPizzaController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
