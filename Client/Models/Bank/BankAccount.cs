5609039c01334361067b6bd10352d0be6c0e36e6
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-26 11:07:16 +0600

    Add Bank UI

---
+++ Client/Models/Bank/BankAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Models.Bank;
+
+public class BankAccount
+{
+    public int Id { get; set; }
+    public string HolderName { get; set; } = string.Empty;
+    public double SavingsAccount { get; set; }
+    public double CheckingsAccount { get; set; }
+}
