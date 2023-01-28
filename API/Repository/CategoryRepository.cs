03ffe55ff597289d5a8a4775ee4ffbd3010f00c0
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-28 23:58:54 +0600

    food, Category Repositorydone

---
+++ API/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Data;
+using API.Models;
+using API.Repository.IRepository;
+
+namespace API.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private readonly AppDbContext _db;
+    public CategoryRepository(AppDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public async Task<Category> UpdateAsync(Category entity)
+    {
+        _db.Categories.Update(entity);
+        await _db.SaveChangesAsync();
+        return entity;
+    }
+}
