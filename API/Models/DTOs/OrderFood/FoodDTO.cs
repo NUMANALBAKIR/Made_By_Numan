c40cddb5b6be451cc9546254ff15eb97c72fb9ac
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-01-31 19:55:56 +0600

    Add SD

--- API/Models/DTOs/OrderFood/FoodDTO.cs
+++ API/Models/DTOs/OrderFood/FoodDTO.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace API.Models.DTOs.OrderFood;
+﻿namespace API.Models.DTOs.OrderFood;
 
 public class FoodDTO
 {
