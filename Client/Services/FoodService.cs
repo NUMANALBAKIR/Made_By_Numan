977d8aeb8116ba6e84608543fbc3bafd4325b54f
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-01 03:48:54 +0600

    draft Foodservice

--- Client/Services/FoodService.cs
+++ Client/Services/FoodService.cs
@@ -1,6 +1,70 @@
-﻿namespace Client.Services;
+﻿using Client.Models;
+using Client.Models.OrderFoodDTOs;
+using Client.Services.IServices;
 
-public class FoodService
+namespace Client.Services;
+
+public class FoodService : BaseService, IFoodService
 {
+
+    private string _APIUrl;
+    private readonly IHttpClientFactory _httpClient;
+    public FoodService(IConfiguration configuration, IHttpClientFactory httpClientFactory) : base(httpClientFactory)
+    {
+        _httpClient = httpClientFactory;
+        _APIUrl = configuration.GetValue<string>("ServiceUrls:APIUrl");
+    }
+
+    public Task<T> CreateAsync<T>(FoodCreateDTO dto, string token)
+    {
+        return SendAsync<T>(new APIRequest()
+        {
+            ApiType = Utility.StaticDetails.APIType.POST,
+            Url = _APIUrl + "/api/FoodsAPI",
+            Token = token,
+            Data = dto
+        });
+    }
+
+    public Task<T> DeleteAsync<T>(int id, string token)
+    {
+        return SendAsync<T>(new APIRequest()
+        {
+            ApiType = Utility.StaticDetails.APIType.DELETE,
+            Url = _APIUrl + "/api/FoodsAPI" + id,
+            Token = token,
+        });
+    }
+
+    public Task<T> GetAllAsync<T>(string token)
+    {
+        return SendAsync<T>(new APIRequest()
+        {
+            ApiType = Utility.StaticDetails.APIType.GET,
+            Url = _APIUrl + "/api/FoodsAPI",
+            Token = token
+        });
+    }
+
+    public Task<T> GetAsync<T>(int id, string token)
+    {
+        return SendAsync<T>(new APIRequest()
+        {
+            ApiType = Utility.StaticDetails.APIType.GET,
+            Url = _APIUrl + "/api/FoodsAPI/" + id,
+            Token = token
+        });
+    }
+
+    public Task<T> UpdateAsync<T>(FoodUpdateDTO dto, string token)
+    {
+        return SendAsync<T>(new APIRequest()
+        {
+            ApiType = Utility.StaticDetails.APIType.PUT,
+            Url = _APIUrl + "/api/FoodsAPI/" + dto.FoodId,
+            Token = token,
+            Data = dto
+        });
+    }
 
 }
