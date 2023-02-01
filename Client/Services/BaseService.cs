8348fce6889966259dff46bee75c1b42a824a271
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-02 00:39:39 +0600

    clarify BaseService

--- Client/Services/BaseService.cs
+++ Client/Services/BaseService.cs
@@ -17,12 +17,12 @@ public class BaseService : IBaseService
         httpClient = httpClientFactory;
     }
 
-    public APIResponse apiResponse { get; set; }
+    public APIResponse apiResponse { get; set; } // ??
     public async Task<T> SendAsync<T>(APIRequest apiRequest)
     {
         try
         {
-            // Request processing
+            // Request processing.
             HttpRequestMessage requestMessage = new();
             requestMessage.Headers.Add("Accept", "application/json");
             requestMessage.RequestUri = new Uri(apiRequest.Url);
@@ -53,29 +53,35 @@ public class BaseService : IBaseService
                 client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", apiRequest.Token);
             }
 
-            // Response processing
+            // Response processing.
             HttpResponseMessage responseMessage = await client.SendAsync(requestMessage);
             var responseString = await responseMessage.Content.ReadAsStringAsync();
 
             try
             {
+                // responseString => Deserialize to APIResponse
                 APIResponse APIResponse = JsonConvert.DeserializeObject<APIResponse>(responseString);
 
-                if (APIResponse != null && (responseMessage.StatusCode == HttpStatusCode.BadRequest
-                    || responseMessage.StatusCode == HttpStatusCode.NotFound))
+                // there's a response, but it's not what we want.
+                if (APIResponse != null &&
+                    (responseMessage.StatusCode == HttpStatusCode.BadRequest || responseMessage.StatusCode == HttpStatusCode.NotFound))
                 {
                     APIResponse.StatusCode = HttpStatusCode.BadRequest;
                     APIResponse.IsSuccess = false;
-                    var res = JsonConvert.SerializeObject(APIResponse);
+                    // APIResponse => Serialize to json => Deserialize to T
+                    var res = JsonConvert.SerializeObject(APIResponse); //??
                     var returnObj = JsonConvert.DeserializeObject<T>(res);
                     return returnObj;
                 }
             }
             catch (Exception ex)
             {
+                // responseString => Deserialize to T
                 var exceptionResponse = JsonConvert.DeserializeObject<T>(responseString);
                 return exceptionResponse;
             }
+
+            // responseString => Deserialize to T
             var apiResponse = JsonConvert.DeserializeObject<T>(responseString);
             return apiResponse;
         }
@@ -83,9 +89,10 @@ public class BaseService : IBaseService
         {
             var dto = new APIResponse
             {
-                ErrorMessages = new List<string> { Convert.ToString(ex.Message) },
+                ErrorMessages = new List<string>() { Convert.ToString(ex.Message) },
                 IsSuccess = false
             };
+            // APIResponse => Serialize to json => Deserialize to T
             var res = JsonConvert.SerializeObject(dto);
             var APIResponse = JsonConvert.DeserializeObject<T>(res);
             return APIResponse;
