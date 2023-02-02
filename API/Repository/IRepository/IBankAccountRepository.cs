83946afc1de46302ecc0a90a32c63f08424df6f4
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-02 22:39:44 +0600

    add

---
+++ API/Repository/IRepository/IBankAccountRepository.cs
@@ -0,0 +1,8 @@
+﻿using API.Models.Bank;
+
+namespace API.Repository.IRepository;
+
+public interface IBankAccountRepository : IRepository<BankAccount>
+{
+    Task<BankAccount> UpdateAsync(BankAccount entity);
+}
