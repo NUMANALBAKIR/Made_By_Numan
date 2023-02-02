b216d5ccb0f2c54a73e53e94a466f349eeec4a11
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-02 22:49:12 +0600

    apir esponse cleanup

--- API/Models/APIResponse.cs
+++ API/Models/APIResponse.cs
@@ -6,5 +6,5 @@ public class APIResponse
 {
     public bool IsSuccess { get; set; } = true;
     public object? Data { get; set; }
-    public List<string>? Messages { get; set; } = new List<string>();
+    public string Message { get; set; }
 }
