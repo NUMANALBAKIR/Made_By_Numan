e3978bd6b1d5f620022d5f00c8d024c9fddd49c9
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-11 04:56:38 +0600

    add appusers

---
+++ API/Models/AppUser/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models.AppUser;
+
+public class AppUser : IdentityUser
+{
+    [Required, Display(Name = "User Name")]
+    public string Name { get; set; }
+    public string Address { get; set; }
+}
