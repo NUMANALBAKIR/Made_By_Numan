7853505bff23ded438af4f4ca013c14d5f527a12
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-12 01:47:30 +0600

    count validation

--- Client/Program.cs
+++ Client/Program.cs
@@ -73,7 +73,7 @@ app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{area=Guest}/{controller=Chart}/{action=Index}/{id?}"
+    pattern: "{area=Guest}/{controller=OrderFood}/{action=Index}/{id?}"
 );
 
 app.Run();
