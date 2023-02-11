66184652024df7679aac266748fdf39f8de24448
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-11 21:05:47 +0600

    add AppUser to models and dtos

--- API/Models/OrderFoodDTOs/CartItemDTO.cs
+++ API/Models/OrderFoodDTOs/CartItemDTO.cs
@@ -1,12 +1,19 @@
-﻿namespace API.Models.OrderFoodDTOs;
+﻿using API.Models.User;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models.OrderFoodDTOs;
 
 public class CartItemDTO
 {
-    // add AppUser
-
     public int CartItemId { get; set; }
 
+    public string AppUserId { get; set; }
+    [ValidateNever, ForeignKey(nameof(AppUserId))]
+    public AppUser AppUser { get; set; }
+
     public int FoodId { get; set; }
+    [ValidateNever, ForeignKey(nameof(FoodId))]
     public FoodDTO Food { get; set; }
 
     public double CurrentPrice { get; set; }
