a6e9bc6b33402d92671346b75ed60e24d9a752eb
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-15 04:09:30 +0600

    before experiemention

--- Client/Models/BankDTOs/BankAccountDTO.cs
+++ Client/Models/BankDTOs/BankAccountDTO.cs
@@ -1,5 +1,6 @@
 ﻿using Client.Models.User;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -14,9 +15,12 @@ public class BankAccountDTO
     public AppUser AppUser { get; set; }
 
     public string HolderName { get; set; }
+    [Precision(18, 2)]
     public double SavingsBalance { get; set; }
+    [Precision(18, 2)]
     public double CheckingsBalance { get; set; }
 
+    [Precision(18, 2)]
     [Range(1, 500, ErrorMessage = "Amount must be between 1 and 500.")]
     public double TransactionAmount { get; set; }
 }
