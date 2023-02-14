583909dc8f1980ffa3cfae2aee9df185af82faf0
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-15 04:09:30 +0600

    before experiemention

---
+++ API/Migrations/20230214221041_addprecision.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addprecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TransactionAmount",
+                table: "BankAccounts",
+                type: "float(18)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SavingsBalance",
+                table: "BankAccounts",
+                type: "float(18)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CheckingsBalance",
+                table: "BankAccounts",
+                type: "float(18)",
+                precision: 18,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TransactionAmount",
+                table: "BankAccounts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(18)",
+                oldPrecision: 18,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "SavingsBalance",
+                table: "BankAccounts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(18)",
+                oldPrecision: 18,
+                oldScale: 2);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "CheckingsBalance",
+                table: "BankAccounts",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float(18)",
+                oldPrecision: 18,
+                oldScale: 2);
+        }
+    }
+}
