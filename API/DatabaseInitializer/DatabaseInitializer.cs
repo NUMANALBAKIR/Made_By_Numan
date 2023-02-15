a90c66895e71a20216de97afd8ffe3f1adc2e9f5
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-15 18:28:32 +0600

    Add DatabaseInitializer

--- API/DatabaseInitializer/DatabaseInitializer.cs
+++ API/DatabaseInitializer/DatabaseInitializer.cs
@@ -14,80 +14,82 @@ public class DatabaseInitializer : IDatabaseInitializer
     }
 
     // Add Food, Category lists to Db
-    public async Task Initialize()
+    public void Initialize()
     {
-        // add foods
-        if (await _db.Foods.CountAsync() == 0)
+        // Add categories
+        if (_db.Categories.Count() == 0)
         {
-            await _db.Foods.AddAsync(new Food
+            _db.Categories.Add(new Category
+            {
+                Name = "Starters"
+            });
+            _db.Categories.Add(new Category
+            {
+                Name = "Breakfast"
+            });
+            _db.Categories.Add(new Category
+            {
+                Name = "Lunch"
+            });
+            _db.Categories.Add(new Category
+            {
+                Name = "Dinner"
+            });
+            _db.SaveChanges();
+        }
+
+        // add foods
+        if (_db.Foods.Count() == 0)
+        {
+            _db.Foods.Add(new Food
             {
                 Name = "Magnum Tiste",
                 Description = "Lorem, deren, trataro, filede, nerada",
                 Price = 5.95M,
                 ImageURL = "assets/img/menu/menu-item-1.png",
-                CategoryId = 0
+                CategoryId = 1
             });
-            await _db.Foods.AddAsync(new Food
+            _db.Foods.Add(new Food
             {
                 Name = "Aut Luia",
                 Description = "deren, trataro, filede, nerada, Lorem",
                 Price = 14.95M,
                 ImageURL = "assets/img/menu/menu-item-2.png",
-                CategoryId = 0
+                CategoryId = 1
             });
-            await _db.Foods.AddAsync(new Food
+            _db.Foods.Add(new Food
             {
                 Name = "Est Eligendi",
                 Description = "trataro, filede, nerada, Lorem, deren",
                 Price = 8.95M,
                 ImageURL = "assets/img/menu/menu-item-3.png",
-                CategoryId = 1
+                CategoryId = 2
             });
-            await _db.Foods.AddAsync(new Food
+            _db.Foods.Add(new Food
             {
                 Name = "Eos Luibusdam",
                 Description = "filede, nerada, Lorem, deren, trataro",
                 Price = 12.95M,
                 ImageURL = "assets/img/menu/menu-item-4.png",
-                CategoryId = 2
+                CategoryId = 3
             });
-            await _db.Foods.AddAsync(new Food
+            _db.Foods.Add(new Food
             {
                 Name = "Luibusdam Eos",
                 Description = "nerada, Lorem, deren, trataro, filede",
                 Price = 10.0M,
                 ImageURL = "assets/img/menu/menu-item-5.png",
-                CategoryId = 2
+                CategoryId = 3
             });
-            await _db.Foods.AddAsync(new Food
+            _db.Foods.Add(new Food
             {
                 Name = "Laboriosam Direva",
                 Description = "polao, nerada, Lorem, deren, trataro",
                 Price = 9.95M,
                 ImageURL = "assets/img/menu/menu-item-6.png",
-                CategoryId = 3
+                CategoryId = 4
             });
-        }
-
-        // Add categories
-        if (await _db.Categories.CountAsync() == 0)
-        {
-            await _db.Categories.AddAsync(new Category
-            {
-                Name = "Starters"
-            });
-            await _db.Categories.AddAsync(new Category
-            {
-                Name = "Breakfast"
-            });
-            await _db.Categories.AddAsync(new Category
-            {
-                Name = "Lunch"
-            });
-            await _db.Categories.AddAsync(new Category
-            {
-                Name = "Dinner"
-            });
+            _db.SaveChanges();
         }
         return;
     }
--- API/DatabaseInitializer/IDatabaseInitializer.cs
+++ API/DatabaseInitializer/IDatabaseInitializer.cs
@@ -2,5 +2,5 @@
 
 public interface IDatabaseInitializer
 {
-    Task Initialize();
+    void Initialize();
 }
