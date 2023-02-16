2356d40491d6f42a0b74434835e5123424d09073
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-16 20:59:30 +0600

    add TransactionsAPIController

--- API/Repository/IRepository/IUnitOfWork.cs
+++ API/Repository/IRepository/IUnitOfWork.cs
@@ -3,6 +3,7 @@
 public interface IUnitOfWork
 {
     IBankAccountRepository BankAccountRepo { get; }
+    ITransactionRepository TransactionRepo { get; }
 
     IFoodRepository FoodRepo { get; }
     ICategoryRepository CategoryRepo { get; }
