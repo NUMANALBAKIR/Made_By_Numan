e87fd8ed1d3bdf00930cc8656a555e1aeda790f5
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-02-16 20:54:19 +0600

    done transaction models and dtos

---
+++ API/Repository/TransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Data;
+using API.Models.Bank;
+using API.Repository.IRepository;
+
+namespace API.Repository;
+
+public class TransactionRepository : Repository<Transaction>, ITransactionRepository
+{
+    private readonly AppDbContext _db;
+    public TransactionRepository(AppDbContext db) : base(db)
+    {
+        _db = db;
+    }
+
+    public async Task<Transaction> UpdateAsync(Transaction entity)
+    {
+        _db.Transactions.Update(entity);
+        await _db.SaveChangesAsync();
+        return entity;
+    }
+}
