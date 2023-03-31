4b62625b18bbb9bc1beb412f85d38e1187694600
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-03-31 22:24:57 +0600

    add SubjectsListsService, CountriesService

--- Client-Angular/src/app/countries.service.ts
+++ Client-Angular/src/app/countries.service.ts
@@ -1,9 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Country } from './country';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CountriesService {
 
-  constructor() { }
+  private _apiUrl: string = 'http://localhost:5091/api/CountriesAPI';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountry(countryId: number): Observable<Country> {
+    return this.httpClient.get<Country>(this._apiUrl + '/' + countryId, { responseType: 'json' });
+  }
+
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(this._apiUrl, { responseType: 'json' })
+      .pipe(map(
+        (data: Country[]) => {          
+          return data;
+        }
+      ));
+  }
+
+
+  addCountry(countryToAdd: Country): Observable<Country> {
+    return this.httpClient.post<Country>(this._apiUrl, countryToAdd, { responseType: 'json' });
+  }
+
+  editCountry(countryToEdit: Country): Observable<Country> {
+    return this.httpClient.put<Country>(this._apiUrl + '/' + countryToEdit.countryId, countryToEdit, { responseType: 'json' });
+  }
+
+  deleteCountry(countryId: number): Observable<string> {
+    return this.httpClient.delete<string>(this._apiUrl + '/' + countryId, { responseType: 'json' });
+  }
+
+  searchCountries(searchBy: string, searchText: string): Observable<Country[]> {
+    let url = this._apiUrl + '/' + searchBy + '/' + searchText;
+    // debugger;
+    return this.httpClient.get<Country[]>(url, { responseType: 'json' });
+  }
 }
--- Client-Angular/src/app/subjects-lists.service.ts
+++ Client-Angular/src/app/subjects-lists.service.ts
@@ -1,9 +1,51 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { SubjectsList } from './subjects-list';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubjectsListsService {
 
-  constructor() { }
+  private _apiUrl: string = 'http://localhost:5091/api/SubjectsListsAPI';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountry(subjectsListId: number): Observable<SubjectsList> {
+    return this.httpClient.get<SubjectsList>(this._apiUrl + '/' + subjectsListId, { responseType: 'json' });
+  }
+
+
+  getSubjectsLists(): Observable<SubjectsList[]> {
+    return this.httpClient.get<SubjectsList[]>(this._apiUrl, { responseType: 'json' })
+      .pipe(map(
+        (data: SubjectsList[]) => {
+          
+          return data;
+        }
+      ));
+  }
+
+
+  addCountry(subjectsListToAdd: SubjectsList): Observable<SubjectsList> {
+    return this.httpClient.post<SubjectsList>(this._apiUrl, subjectsListToAdd, { responseType: 'json' });
+  }
+
+  editCountry(subjectsListToEdit: SubjectsList): Observable<SubjectsList> {
+    return this.httpClient.put<SubjectsList>(this._apiUrl + '/' + subjectsListToEdit.subjectsListId, subjectsListToEdit, { responseType: 'json' });
+  }
+
+  deleteCountry(subjectsListId: number): Observable<string> {
+    return this.httpClient.delete<string>(this._apiUrl + '/' + subjectsListId, { responseType: 'json' });
+  }
+
+  searchSubjectsLists(searchBy: string, searchText: string): Observable<SubjectsList[]> {
+    let url = this._apiUrl + '/' + searchBy + '/' + searchText;
+    // debugger;
+    return this.httpClient.get<SubjectsList[]>(url, { responseType: 'json' });
+  }
 }
