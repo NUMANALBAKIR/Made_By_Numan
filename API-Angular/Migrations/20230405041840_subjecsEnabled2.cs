2a1069eadecdaae14b0103b5c6cee27b7cfefc6b
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-04-05 10:19:47 +0600

    subjects table migration done

---
+++ API-Angular/Migrations/20230405041840_subjecsEnabled2.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_Angular.Migrations
+{
+    /// <inheritdoc />
+    public partial class subjecsEnabled2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
