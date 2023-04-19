e10e9fa05ff7fb8cd6903585d2e15c8d18145178
Numan Al Bakir <36388982+NUMANALBAKIR@users.noreply.github.com>
2023-04-20 01:43:08 +0600

    add filter pipe

--- Client-Angular/src/app/admin/filter.pipe.ts
+++ Client-Angular/src/app/admin/filter.pipe.ts
@@ -8,15 +8,23 @@ import { Student } from '../Student';
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(value: any[], searchBy: string, searchText: string): any {
-    if (value == null) {
-      return value;
+  transform(studentsArr: any[], searchBy: string, searchText: string): any {
+
+    searchBy = searchBy.toLowerCase();
+    searchText = searchText.toLowerCase();
+
+    if (studentsArr == null) {
+      return studentsArr;
     }
 
     let resultArray = [];
-    for (let item of value) {
-      if (String(item[searchBy]).toLowerCase().indexOf(searchText.toLowerCase()) >= 0) {
-        resultArray.push(item);
+
+    for (let student of studentsArr) {
+
+      let valueToCompare = String(student[searchBy]).toLowerCase();
+
+      if (valueToCompare.indexOf(searchText) != -1) {
+        resultArray.push(student);
       }
     }
     return resultArray;
